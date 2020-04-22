9b7c7b33aa3682bf4f155e1b8924bb1d780dd82c
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-04-22 13:05:34 -0400

    update

--- leiwand.py
+++ leiwand.py
@@ -27,23 +27,31 @@ variables = {
     "color0": "{RGB}{0,0,204}",
     "color1": "{RGB}{0,204,0}",
     "color2": "{RGB}{204,0,0}",
-    "vertexcolor": "{RGB}{0,0,0}",
-    "fontcolor": "{RGB}{250,250,250}",
-    "bend00":"0",
-    "bend11":"10",
-    "bend22":"-10",
-    "bend01":"20",
-    "bend10":"-20",
-    "bend02":"30",
-    "bend20":"-30",
-    "bend12":"40",
-    "bend21":"-40",
-    "angle":180
+    "vertexcolor": "{RGB}{250,250,250}",
+    "fontcolor": "{RGB}{0,0,0}",
+    "bend00":"-5",
+    "bend11":"5",
+    "bend22":"0",
+    "bend01":"-10",
+    "bend10":"10",
+    "bend02":"-15",
+    "bend20":"15",
+    "bend12":"-20",
+    "bend21":"20",
+    "angle":180,
+    "vertices": None,
+    "whitespace": "10pt"
 }
 
 print(docstring)
 for k,v in variables.items():
     print("{}={}".format(k,v))
+
+print("""
+Additional options are:
+bendall=value  | set all bendxx angles to same value
+"""
+)
 
 filename = "data.txt"
 output = "graph"
@@ -55,6 +63,21 @@ for arg in sys.argv:
     elif "=" in arg:
         tmp = arg.split("=")
         variables[tmp[0]] = tmp[1]
+
+if "bendall" in variables:
+    for x in ["00", "11", "22", "01", "10", "02", "20", "21", "12"]:
+        variables["bend"+x] = variables["bendall"]
+
+external_vertices = None
+if variables['vertices'] is not None:
+    external_vertices = variables["vertices"].split(' ')
+    # reverse order (drawing is counter-clockwise)
+    external_vertices = list(reversed(external_vertices))
+    print("got vertices: ", external_vertices)
+
+whitespace = None
+if variables["whitespace"] is not None:
+    whitespace = variables["whitespace"]
 
 with open(output + ".tex", "w") as outf:
     data = []
@@ -68,7 +91,11 @@ with open(output + ".tex", "w") as outf:
     else:
         with open(filename, "r") as f:
             for line in f:
+                if line == "\n":
+                    print("caught blanc line")
+                    continue
                 tmp = line.split(",")
+                print("tmp=", tmp, " tmp0=", tmp[0])
                 data.append((float(tmp[0]), str(tmp[1]).strip(), int(tmp[2]), str(tmp[3]).strip(), int(tmp[4])))
 
     optionmap = {
@@ -82,9 +109,12 @@ with open(output + ".tex", "w") as outf:
         (1, 2): "bicolor={onecol}{twocol}, bend left="+variables["bend12"],
         (2, 1): "bicolor={twocol}{onecol}, bend right="+variables["bend21"],
     }
-
+    
+    if whitespace is not None:
+        print("\documentclass[border={}]{}".format(whitespace, r"{standalone}"), file=outf)
+    else:
+        print(r"\documentclass{standalone}", file=outf)
     print(r"""
-    \documentclass{standalone}
     
     \usepackage{tikz}
     \usepackage{verbatim}
@@ -134,10 +164,18 @@ with open(output + ".tex", "w") as outf:
     vertices = list(set(vertices))
     max_weight = max(weights)
 
-    poly = Polygon.regular(len(vertices), radius=5, angle=int(variables["angle"]))
+    
+    # check if vertices where specified manually
+    if external_vertices is not None:
+        print("replacing: ", vertices)
+        print("with external vertices: ", external_vertices)
+        vertices = external_vertices
+    else:
+        #sort vertices alphabetically
+        vertices = list(reversed(sorted(vertices)))
 
-    # sort vertices alphabetically
-    vertices = list(reversed(sorted(vertices)))
+    poly = Polygon.regular(len(vertices), radius=5, angle=float(variables["angle"]))
+
     for i, coord in enumerate(poly):
         print(coord)
         print(r"\node[vertex] ({name}) at ({x},{y}) {xname};".format(name=vertices[i], xname=r"{\color{fontcolor}" + vertices[i] + "}",
